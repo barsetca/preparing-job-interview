db3319029cb7ba52e2747b2eca16660a0e1309b9
Viktor Cherniak <barsetca@gmail.com>
2021-01-30 15:49:22 +0300

    Initial commit

---
+++ src/main/java/com/preparing/job/interview/PreparingJobInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.preparing.job.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparingJobInterviewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PreparingJobInterviewApplication.class, args);
+  }
+
+}
