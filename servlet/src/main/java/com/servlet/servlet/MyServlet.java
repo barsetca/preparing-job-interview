fccade8087ade7e381d21beff53b4512df7ad9b2
Viktor Cherniak <barsetca@gmail.com>
2021-02-18 15:08:15 +0300

    deleted @ for class in MyServlet

--- servlet/src/main/java/com/servlet/servlet/MyServlet.java
+++ servlet/src/main/java/com/servlet/servlet/MyServlet.java
@@ -3,12 +3,11 @@ package com.servlet.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "MyServlet", value = "/MyServlet")
+
 public class MyServlet extends HttpServlet {
 
   @Override
